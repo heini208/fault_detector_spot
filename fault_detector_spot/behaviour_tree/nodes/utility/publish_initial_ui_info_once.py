2c677b248a84b7288623bf40b99a77d85b27cc93
heini208 <mstemmeler@gmx.de>
2025-09-10 19:00:10 +0200

    improve publish once reliability ( ui sometimes missed it)

--- fault_detector_spot/behaviour_tree/nodes/utility/publish_initial_ui_info_once.py
+++ fault_detector_spot/behaviour_tree/nodes/utility/publish_initial_ui_info_once.py
@@ -1,38 +1,41 @@
 import os
+
 import py_trees
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python import get_package_share_directory
 from fault_detector_msgs.msg import StringArray
 from fault_detector_spot.behaviour_tree.QOS_PROFILES import LATCHED_QOS
 
-class PublishInitialUIInfoOnce(py_trees.behaviour.Behaviour):
-    """
-    Publishes all existing maps in the recordings folder once at startup, then always returns SUCCESS.
-    """
 
+class PublishInitialUIInfoOnce(py_trees.behaviour.Behaviour):
     def __init__(self, name: str = "PublishInitialMapListOnce"):
         super().__init__(name)
         self.recordings_dir = os.path.join(
             get_package_share_directory("fault_detector_spot"),
             "maps"
         )
-        self._has_run = False
+        self._published_once = False
 
     def setup(self, **kwargs):
         node = kwargs.get("node")
         if node is None:
             raise RuntimeError("Node must be passed to setup() for ROS publishing")
+        self.node = node
         self.publisher = node.create_publisher(StringArray, "map_list", LATCHED_QOS)
 
     def update(self) -> py_trees.common.Status:
-        if not self._has_run:
+        if self._published_once:
+            return py_trees.common.Status.SUCCESS
+
+        # Only publish if at least one subscriber exists
+        if self.publisher.get_subscription_count() > 0:
             self.publish_map_list()
-            self._has_run = True
-        return py_trees.common.Status.SUCCESS
+            self._published_once = True
+            return py_trees.common.Status.SUCCESS
+        else:
+            # Keep ticking until a subscriber appears
+            return py_trees.common.Status.SUCCESS
 
     def publish_map_list(self):
-        if self.publisher is None:
-            return
-
         map_files = []
         if os.path.isdir(self.recordings_dir):
             for f in sorted(os.listdir(self.recordings_dir)):
@@ -42,4 +45,3 @@ class PublishInitialUIInfoOnce(py_trees.behaviour.Behaviour):
         msg = StringArray()
         msg.names = map_files
         self.publisher.publish(msg)
-
