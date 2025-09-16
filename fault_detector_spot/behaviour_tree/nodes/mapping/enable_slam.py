8f47934df8fcbed75fe6f247380a8ec030ad91b3
heini208 <mstemmeler@gmx.de>
2025-09-16 14:50:35 +0200

    Add keep slam behaviour running until ready

--- fault_detector_spot/behaviour_tree/nodes/mapping/enable_slam.py
+++ fault_detector_spot/behaviour_tree/nodes/mapping/enable_slam.py
@@ -1,23 +1,49 @@
+import typing
 import py_trees
 
 from fault_detector_spot.behaviour_tree.nodes.mapping.slam_toolbox_helper import SlamToolboxHelper
+from nav_msgs.msg import OccupancyGrid
 
 
 class EnableSLAM(py_trees.behaviour.Behaviour):
-    def __init__(self,slam_helper: SlamToolboxHelper ,name: str = "EnableSLAM"):
+    def __init__(self, slam_helper: SlamToolboxHelper, name: str = "EnableSLAM"):
         super().__init__(name)
         self.slam_helper = slam_helper
         self.blackboard = self.attach_blackboard_client(name=name)
         self.blackboard.register_key("active_map_name", access=py_trees.common.Access.READ)
+
+        self.launched_initialized = False
+        self.map_received_after_launch = False
+
+    def setup(self, **kwargs: typing.Any) -> None:
+        self.node = kwargs.get("node")
+        self.sub = self.node.create_subscription(
+            OccupancyGrid,
+            "/map",
+            self._map_callback,
+            10,
+        )
+
+    def _map_callback(self, msg: OccupancyGrid):
+        # only set to True if we’re in the launched state
+        if self.launched_initialized:
+            self.map_received_after_launch = True
 
     def update(self) -> py_trees.common.Status:
         if not self.blackboard.active_map_name:
             self.feedback_message = "No active map set, cannot enable SLAM"
             return py_trees.common.Status.FAILURE
 
-        self.feedback_message = f"Launching Mapping"
-        self.slam_helper.start_mapping_from_existing()
-        if self.slam_helper.is_slam_running():
+        if not self.launched_initialized:
+            self.feedback_message = "Launching Mapping"
+            self.launched_initialized = True
+            self.map_received_after_launch = False
+            self.slam_helper.start_mapping_from_existing()
+            return py_trees.common.Status.RUNNING
+        elif self.slam_helper.is_slam_running() and self.map_received_after_launch:
+            self.feedback_message = "Mapping enabled (SLAM running)"
+            self.launched_initialized = False
             return py_trees.common.Status.SUCCESS
-        else:
-            return py_trees.common.Status.FAILURE
+
+        self.feedback_message = "Waiting for map updates..."
+        return py_trees.common.Status.RUNNING
