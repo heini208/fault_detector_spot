399c0ee247b7e008e63b5374c1f9a7447cdf5b6c
heini208 <mstemmeler@gmx.de>
2026-02-06 15:19:14 +0100

    remove depricated code

--- fault_detector_spot/behaviour_tree/nodes/utility/helper_initializer.py
+++ fault_detector_spot/behaviour_tree/nodes/utility/helper_initializer.py
@@ -1,7 +1,5 @@
 import py_trees
 from fault_detector_spot.behaviour_tree.nodes.mapping.rtab_helper import RTABHelper
-from fault_detector_spot.behaviour_tree.nodes.mapping.slam_toolbox_helper import SlamToolboxHelper
-
 
 class HelperInitializer(py_trees.behaviour.Behaviour):
     """
@@ -20,22 +18,13 @@ class HelperInitializer(py_trees.behaviour.Behaviour):
         self.bb_client = self.attach_blackboard_client()
         # register any keys your helpers need
 
-        if self.use_simulation:
-            self.slam_helper = SlamToolboxHelper(
-                node=self.node,
-                blackboard=self.bb_client,
-                launch_file="slam_sim_merged_launch.py",
-                nav2_launch_file="nav2_sim_launch.py",
-                nav2_params_file="nav2_sim_params.yaml"
-            )
-        else: # real robot
-            self.slam_helper = RTABHelper(
-                node=self.node,
-                blackboard=self.bb_client,
-                launch_file="rtab_mapping_launch.py",
-                nav2_launch_file="nav2_spot_launch.py",
-                nav2_params_file="nav2_spot_params.yaml"
-            )
+        self.slam_helper = RTABHelper(
+            node=self.node,
+            blackboard=self.bb_client,
+            launch_file="rtab_mapping_launch.py",
+            nav2_launch_file="nav2_spot_launch.py",
+            nav2_params_file="nav2_spot_params.yaml"
+        )
 
         self.nav2_helper = self.slam_helper.nav2_helper
 
