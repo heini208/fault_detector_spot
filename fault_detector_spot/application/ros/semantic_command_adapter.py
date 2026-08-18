d2d17d0062ce3c3c16542d33f26dfff5aeccf05c
Marcel Stemmeler <57675018+heini208@users.noreply.github.com>
2026-08-18 20:29:29 +0200

    propagate close-surface starting distance

--- fault_detector_spot/application/ros/semantic_command_adapter.py
+++ fault_detector_spot/application/ros/semantic_command_adapter.py
@@ -77,6 +77,9 @@ def semantic_command_from_message(
         orientation_mode=message.orientation_mode,
         wait_time=message.wait_time,
         target_surface_distance_m=message.target_surface_distance_m,
+        aligned_preapproach_distance_m=(
+            message.aligned_preapproach_distance_m
+        ),
         map_name=message.map_name,
         waypoint_name=message.waypoint_name,
         inspection=InspectionSelection(
@@ -107,6 +110,9 @@ def semantic_command_to_message(
     message.wait_time = float(command.wait_time)
     message.target_surface_distance_m = float(
         command.target_surface_distance_m
+    )
+    message.aligned_preapproach_distance_m = float(
+        command.aligned_preapproach_distance_m
     )
     message.map_name = command.map_name
     message.waypoint_name = command.waypoint_name
